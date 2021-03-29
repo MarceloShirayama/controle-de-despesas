33c764b626badfdb18475b763cb2331aa35fd17f
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-03-29 16:57:04 -0300

    refactored code

--- script.js
+++ script.js
@@ -20,15 +20,15 @@ const removeTransaction = (id) => {
   init();
 };
 
-const addTransactionsIntoDOM = (transaction) => {
-  const operator = transaction.amount > 0 ? '+' : '-';
-  const CSSClass = transaction.amount > 0 ? 'plus' : 'minus';
-  const amountWithoutOperator = Math.abs(transaction.amount);
+const addTransactionsIntoDOM = ({ name, amount, id }) => {
+  const operator = amount > 0 ? '+' : '-';
+  const CSSClass = amount > 0 ? 'plus' : 'minus';
+  const amountWithoutOperator = Math.abs(amount);
   const li = document.createElement('li');
   li.classList.add(CSSClass);
-  li.innerHTML = `${transaction.name}\
-    <span>${operator} R$ ${amountWithoutOperator}</span>\
-    <button class="delete-btn" onClick="removeTransaction(${transaction.id})">\
+  li.innerHTML = `${name}\
+    <span>${operator} R$ ${amountWithoutOperator.toFixed(2)}</span>\
+    <button class="delete-btn" onClick="removeTransaction(${id})">\
       x\
     </button>
   `;
@@ -36,17 +36,17 @@ const addTransactionsIntoDOM = (transaction) => {
   transactionsUl.append(li);
 };
 
-const updateBalanceValues = () => {
+const getTotalAmount = (transactionsAmount, transactionType) => (transactionsAmount
+  .filter((amount) => (transactionType === 'income' ? amount > 0 : amount < 0))
+  .reduce((acc, amount) => acc + amount, 0)
+);
+
+const insertBalanceValues = () => {
   const transactionsAmount = transactions
     .map((transaction) => transaction.amount);
 
-  const income = transactionsAmount
-    .filter((amount) => amount > 0)
-    .reduce((acc, amount) => acc + amount, 0);
-
-  const expense = transactionsAmount
-    .filter((amount) => amount < 0)
-    .reduce((acc, amount) => acc + amount, 0);
+  const income = getTotalAmount(transactionsAmount, 'income');
+  const expense = getTotalAmount(transactionsAmount, 'expense');
 
   const currentBalance = (income + expense);
 
@@ -55,19 +55,35 @@ const updateBalanceValues = () => {
   expenseDisplay.textContent = `- R$ ${Math.abs(expense).toFixed(2)}`;
 };
 
+// atualização das transações na tela.
 const init = () => {
   transactionsUl.innerHTML = '';
   transactions.forEach(addTransactionsIntoDOM);
-  updateBalanceValues();
+  insertBalanceValues();
 };
 
 init();
 
+// atualização do local storage.
 const updateLocalStorage = () => (
   localStorage.setItem('transactions', JSON.stringify(transactions))
 );
 
 const generateID = () => Math.round(Math.random() * 1000);
+
+const addCreatedTransaction = (transactionName, transactionAmount) => {
+  transactions.push({
+    id: generateID(),
+    name: transactionName,
+    amount: Number(transactionAmount),
+  });
+};
+
+// limpeza dos inputs.
+const clearInputs = () => {
+  inputTransactionName.value = '';
+  inputTransactionAmount.value = '';
+};
 
 const handleFormSubimit = (event) => {
   // impedir que o form seja enviado e a página recarregada.
@@ -76,33 +92,22 @@ const handleFormSubimit = (event) => {
   // variáveis com os valores inseridos no input.
   const transactionName = inputTransactionName.value.trim();
   const transactionAmount = inputTransactionAmount.value.trim();
+  const checkEmptyInputs = transactionName === '' || transactionAmount === '';
 
-  // verificação se os inputs foram preenchidos e exibindo uma mensagem se não foram preenchidos.
-  if (transactionName === '' || transactionAmount === '') {
+  // verificação se há inputs vazios e exibir uma mensagem se tiver.
+  if (checkEmptyInputs) {
     // eslint-disable-next-line no-alert
     alert('Nome e valor da transação são obrigatórios.');
     return;
   }
 
-  // criando a transação.
-  const transaction = {
-    id: generateID(),
-    name: transactionName,
-    amount: Number(transactionAmount),
-  };
+  addCreatedTransaction(transactionName, transactionAmount);
 
-  // adicionando a transação ao array de transações.
-  transactions.push(transaction);
-
-  // atualização das transações na tela.
   init();
 
-  // atualização do local storage.
   updateLocalStorage();
 
-  // limpeza dos inputs.
-  inputTransactionName.value = '';
-  inputTransactionAmount.value = '';
+  clearInputs();
 };
 
 form.addEventListener('submit', handleFormSubimit);
